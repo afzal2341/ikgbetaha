4a4430c1b3966228ccd3996524c20e92b4bf49f0
afzal2341 <59077318+afzal2341@users.noreply.github.com>
2024-01-12 04:11:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,37 +1,78 @@
 let userScore= 0;
 let compScore= 0;
-
 const choices = document.querySelectorAll(".choice");
 const msg = document.querySelector("#msg");
 const userScorePara= document.querySelector("#user-score"); 
 const compScorePara= document.querySelector("#comp-score");
+const yourPick= document.querySelector(".your-pick");
+const compPick= document.querySelector(".comp-pick");
+
+const displayBothChoices= (userChoice,compChoice)=>{
+    if(userChoice==="rock"){
+        yourPick.innerHTML=
+        `<div id="rock">
+            <img src="rock.jpg" alt="">
+        </div>`
+    }
+    else if(userChoice==="paper"){
+        yourPick.innerHTML=
+        `<div id="paper">
+            <img src="paper.jpg" alt="">
+        </div>`
+    }
+    else{
+        yourPick.innerHTML=
+        `<div id="scissors">
+            <img src="scissors.jpg" alt="">
+        </div>`
+    }
+
+    if(compChoice==="rock"){
+        compPick.innerHTML=
+        `<div id="rock">
+            <img src="rock.jpg" alt="">
+        </div>`
+    }
+    else if(compChoice==="paper"){
+        compPick.innerHTML=
+        `<div id="paper">
+            <img src="paper.jpg" alt="">
+        </div>`
+    }
+    else{
+        compPick.innerHTML=
+        `<div id="scissors">
+            <img src="scissors.jpg" alt="">
+        </div>`
+    }
+}
+        
 //generate computer choice
 const getCompChoice = ()=>{
     options=["rock", "paper", "scissors"];
     const randomIdx= Math.floor(Math.random()*3);
     // console.log(randomIdx);
     return options[randomIdx];
-    
 };
 
 const drawGame= ()=>{
     console.log("Game was draw.");
-    msg.innerText="Game was Draw. Play Again.";
+    msg.innerText="Game was Draw.";
     msg.style.backgroundColor= "#081b31";
-
 }
+
 const showWinner= (userWin,userChoice,compChoice) =>{
     if(userWin){
         userScore++;
         console.log("You win!");
-        msg.innerText=`You Win! Your ${userChoice} beats ${compChoice}`;
+        msg.innerText=`You Win!`;
         msg.style.backgroundColor="green"; 
         userScorePara.innerText=userScore;
     }
     else{
         compScore++;
-        console.log("You lose");
-        msg.innerText=`You lose. ${compChoice} beats your ${userChoice}`;
+        console.log("You loss");
+        msg.innerText=`You loss.`;
         msg.style.backgroundColor="red";
         compScorePara.innerText=compScore;
     }
@@ -42,7 +83,8 @@ const playGame= (userChoice)=>{
     //Generate computer choice
     const compChoice = getCompChoice(); 
     console.log("Comp choice= ",compChoice);
-
+    displayBothChoices(userChoice,compChoice);
+    
     if(userChoice=== compChoice){
         //Draw Game
         drawGame();
@@ -61,64 +103,6 @@ const playGame= (userChoice)=>{
         }
         showWinner(userWin,userChoice,compChoice);
     }
-
-
-
-
-
-    // if(userChoice===compChoice){
-    //     console.log("Game draw");
-    //     //Draw
-
-    //     let msg=document.querySelector("#msg");
-    //     msg.innerText="Match Draw";
-    // }
-    // else{
-    //     if(userChoice==="rock"){
-    //         if(compChoice==="paper"){
-    //             console.log("You loss");
-    //             let msg=document.querySelector("#msg");
-    //             msg.innerText="You Loss";
-    //             //loss
-    //         }
-    //         else{
-    //             console.log("You win");
-    //             let msg=document.querySelector("#msg");
-    //             msg.innerText="You win";
-    //             //win
-    //         }
-    //     }
-    //     if(userChoice==="paper"){
-    //         if(compChoice==="scissors"){
-    //             console.log("You loss");
-    //             let msg=document.querySelector("#msg");
-    //             msg.innerText="You Loss";
-    //             //loss
-    //         }
-    //         else{
-    //             console.log("You win");
-    //             let msg=document.querySelector("#msg");
-    //             msg.innerText="You Win";
-                
-    //             //win
-    //         }
-    //     }
-    //     if(userChoice==="scissors"){
-    //         if(compChoice=="rock"){
-    //             console.log("You loss");
-    //             let msg=document.querySelector("#msg");
-    //             msg.innerText="You Loss";
-    //             //loss
-    //         }
-    //         else{
-    //             console.log("You win");
-    //             let msg=document.querySelector("#msg");
-    //             msg.innerText="You Win";
-                
-    //             //win
-    //         }
-    //     }
-    // }
 };
 
 choices.forEach((choice)=>{
